08f46218300f9000684874bba523817ddadbed3b
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-10-28 22:15:51 +0200

    Added fastify
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
+import {
+  FastifyAdapter,
+  type NestFastifyApplication
+} from "@nestjs/platform-fastify";
 import BoardModule from "./modules/board.module.js";
 
 async function bootstrap() {
-  const app = await NestFactory.create(BoardModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    BoardModule,
+    new FastifyAdapter()
+  );
   await app.listen(3000);
 }
 
