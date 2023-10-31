3bf0904f5fef760e079d27248f259959c5cb9e80
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-10-31 09:10:34 +0100

    Added get board
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- src/features/board/board.controller.ts
+++ src/features/board/board.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get, NotImplementedException } from "@nestjs/common";
-import type Board from "models/board.js";
+import Board from "models/board.js";
 
 @Controller("boards")
 export default class BoardController {
   @Get()
   getAllBoards(): Board[] {
     throw new NotImplementedException("Get boards is not yet implemented!");
+  }
+
+  @Get(":boardid")
+  getBoard(boardId: string): Board {
+    return new Board("name", boardId);
   }
 }
