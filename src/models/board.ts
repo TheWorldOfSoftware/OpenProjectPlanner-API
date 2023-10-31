0075691822ad69377628eb6044aff0a3d037703c
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-10-31 09:10:05 +0100

    Improved board model
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,14 +1,6 @@
 export default class Board {
-  public get id(): string | undefined {
-    return this._id;
-  }
-
-  public get name(): string {
-    return this._name;
-  }
-
   public constructor(
-    private readonly _name: string,
-    private readonly _id?: string
+    public readonly name: string,
+    public readonly id?: string
   ) {}
 }
