07b8e19be77b5d4b21363431aee9378ff639bfef
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-11-10 23:39:16 +0100

    Added dotenv package
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
+import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
 import AppModule from "./app.js";
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   await app.listen(3000);
-}
-
-bootstrap();
+})();
