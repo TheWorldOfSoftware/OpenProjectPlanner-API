b8e801d9d1b2aee99dd69799e880d819a30d21fb
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-12-18 00:04:32 +0100

    refactor: added port
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 const env = z.object({
   MYSQL_CONNECTION: z.string(),
   MYSQL_PASSWORD: z.string(),
-  MYSQL_USER: z.string()
+  MYSQL_USER: z.string(),
+  PORT: z.string()
 });
 
 env.parse(process.env);
